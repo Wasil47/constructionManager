469356fdced847a6e23440699aa4715854df4732
Wasil47 <wasil.mateusz@gmail.com>
2020-08-05 14:22:22 +0200

    test commit

--- src/app/modules/project/components/building-selection/building-selection.component.ts
+++ src/app/modules/project/components/building-selection/building-selection.component.ts
@@ -9,7 +9,7 @@ export class BuildingSelectionComponent implements OnInit {
   selectObiekt = 'Obiekt';
   objects = ['Blok nr.1 ', 'Blok nr.2', 'Blok nr.3'];
   selectFragment = 'Część';
-  fragments = ['Klatka A', 'Klatka C', 'Klatka C'];
+  fragments = ['Klatka A', 'Klatka B', 'Klatka C'];
   selectStorey = 'Kondygnacja';
   storeys = ['-2', '-1', 'parter', '1'];
   // objects = "test"
